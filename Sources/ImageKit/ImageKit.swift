bedc4c083dd1c0146b21b625d12203b75cd01d0b
Kopiev Ivan <x@mustdev.ru>
2023-09-25 18:51:43 +0300

    Initial Commit

---
+++ Sources/ImageKit/ImageKit.swift
@@ -0,0 +1,6 @@
+public struct ImageKit {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/ImageKitTests/ImageKitTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import ImageKit
+
+final class ImageKitTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(ImageKit().text, "Hello, World!")
+    }
+}
